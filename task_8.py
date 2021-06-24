402bfcc05c5be501103675a2575d29fc00f3dc7e
johnk2280 <johnk2280@gmail.com>
2021-06-24 21:14:38 +0500

    [add] added hash_fun, seek_slot

--- task_8.py
+++ task_8.py
@@ -5,14 +5,19 @@ class HashTable:
         self.slots = [None] * self.size
 
     def hash_fun(self, value):
-        # в качестве value поступают строки!
-
-        # всегда возвращает корректный индекс слота
-        return 0
+        return sum(value.encode('utf-8')) % self.size
 
     def seek_slot(self, value):
-        # находит индекс пустого слота для значения, или None
-        return None
+        cash = set()
+        slot_index = self.hash_fun(value)
+        while True:
+            cash.add(slot_index)
+            if not self.slots[slot_index]:
+                return slot_index
+
+            slot_index = (slot_index + self.step) % len(self.slots)
+            if len(cash) == len(self.slots):
+                return None
 
     def put(self, value):
         # записываем значение по хэш-функции
@@ -20,6 +25,11 @@ class HashTable:
         # возвращается индекс слота или None,
         # если из-за коллизий элемент не удаётся
         # разместить
+        slot_index = self.seek_slot(value)
+        if slot_index:
+            self.slots.append(slot_index)
+            return slot_index
+
         return None
 
     def find(self, value):
