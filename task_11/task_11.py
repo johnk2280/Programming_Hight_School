a9c5673c2002e2c45f4d1953703208a2e9011157
johnk2280 <johnk2280@gmail.com>
2021-06-30 22:12:21 +0500

    [add] code refactoring

--- task_11/task_11.py
+++ task_11/task_11.py
@@ -2,25 +2,26 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.storage = [0] * self.filter_len
+        self.filter = 0
 
     def hash1(self, str1):
         result = 0
         for c in str1:
             result = (result * 17 + ord(c)) % self.filter_len
 
-        return result
+        return 1 << result
 
     def hash2(self, str1):
         result = 0
         for c in str1:
             result = (result * 223 + ord(c)) % self.filter_len
 
-        return result
+        return 1 << result
 
     def add(self, str1):
-        self.storage[self.hash1(str1)] = 1
-        self.storage[self.hash2(str1)] = 1
+        self.filter |= self.hash1(str1)
+        self.filter |= self.hash2(str1)
 
     def is_value(self, str1):
-        return False if self.storage[self.hash1(str1)] == 0 or self.storage[self.hash2(str1)] == 0 else True
+        result = self.hash1(str1) + self.hash2(str1)
+        return self.filter & result == result
